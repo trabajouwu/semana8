de737487518934d3c7abb3e6174ddfe97262c801
trabajouwu <87871445+trabajouwu@users.noreply.github.com>
2021-07-29 17:50:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const d = document;
+function hamburguerMenu(panelBtn, panel, menuLink) {
+  d.addEventListener("click", e => {
+    if (e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
+      d.querySelector(panel).classList.toggle("is-active");
+      d.querySelector(panelBtn).classList.toggle("is-active");
+    }
+    if (e.target.matches(menuLink)) {
+      d.querySelector(panel).classList.remove("is-active");
+      d.querySelector(panelBtn).classList.remove("is-active");
+
+    }
+  })
+}
+
+
+d.addEventListener("DOMContentLoaded" , e => { 
+  hamburguerMenu(".hamburger", ".panel", ".menu a");
+});
